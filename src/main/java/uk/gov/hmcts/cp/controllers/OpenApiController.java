9e975d3224552fd37b42e5a3726530ff9221a35b
Dmitri Algazin <Dmitri.Algazin@hmcts.net>
2025-04-15 16:58:57 +0100

    sync with latest openapi spec;

--- src/main/java/uk/gov/hmcts/cp/controllers/OpenApiController.java
+++ src/main/java/uk/gov/hmcts/cp/controllers/OpenApiController.java
@@ -4,18 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.cp.openapi.api.DefaultApi;
+import uk.gov.hmcts.cp.openapi.api.CourtHouseApi;
 import uk.gov.hmcts.cp.openapi.model.CourtHousesschema;
 import uk.gov.hmcts.cp.services.OpenApiService;
 
 @RestController
 @RequiredArgsConstructor
-public class OpenApiController implements DefaultApi {
+public class OpenApiController implements CourtHouseApi {
 
     private final OpenApiService openApiService;
 
     @Override
-    public ResponseEntity<CourtHousesschema> courthousesCourtIdGet(String courtId) {
+    public ResponseEntity<CourtHousesschema> getCourthouseByCourtId(String courtId) {
         CourtHousesschema courtHousesschema = openApiService.getCourtHouse(courtId);
         return new ResponseEntity<>(courtHousesschema, HttpStatus.OK);
     }
