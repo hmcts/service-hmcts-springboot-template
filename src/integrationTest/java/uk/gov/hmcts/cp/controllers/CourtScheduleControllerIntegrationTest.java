7138c11616638d93bc86c7b4912d6f318d1e5011
samirgarg <samir.garg@hmcts.net>
2025-09-11 12:41:44 +0100

    Add configurable JWT token validation and then populating auth details in a request scope object

--- src/integrationTest/java/uk/gov/hmcts/cp/controllers/CourtScheduleControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cp/controllers/CourtScheduleControllerIntegrationTest.java
@@ -2,11 +2,11 @@ package uk.gov.hmcts.cp.controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -22,10 +22,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class CourtScheduleControllerIT {
-    private static final Logger log = LoggerFactory.getLogger(CourtScheduleControllerIT.class);
+class CourtScheduleControllerIntegrationTest {
+    private static final Logger log = LoggerFactory.getLogger(CourtScheduleControllerIntegrationTest.class);
 
-    @Autowired
+    @Resource
     private MockMvc mockMvc;
 
     @Test
